967faa49786a221a2480a144ee4a44ee744879ec
Sam Robbins <samrobbinsgb@gmail.com>
2020-10-10 11:07:44 +0100

    Add files

---
+++ api/_lib/template.ts
@@ -0,0 +1,145 @@
+
+import { readFileSync } from 'fs';
+import marked from 'marked';
+import { sanitizeHtml } from './sanitizer';
+import { ParsedRequest } from './types';
+const twemoji = require('twemoji');
+const twOptions = { folder: 'svg', ext: '.svg' };
+const emojify = (text: string) => twemoji.parse(text, twOptions);
+
+const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
+const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
+const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+
+function getCss(theme: string, fontSize: string) {
+    let background = 'white';
+    let foreground = 'black';
+    let radial = 'lightgray';
+
+    if (theme === 'dark') {
+        background = 'black';
+        foreground = 'white';
+        radial = 'dimgray';
+    }
+    return `
+    @font-face {
+        font-family: 'Inter';
+        font-style:  normal;
+        font-weight: normal;
+        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Inter';
+        font-style:  normal;
+        font-weight: bold;
+        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Vera';
+        font-style: normal;
+        font-weight: normal;
+        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
+      }
+
+    body {
+        background: ${background};
+        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
+        background-size: 100px 100px;
+        height: 100vh;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    .wrap{
+        font-size: ${sanitizeHtml(fontSize)};
+    }
+
+    code {
+        color: #D400FF;
+        font-family: 'Vera';
+        white-space: pre-wrap;
+        letter-spacing: -5px;
+    }
+
+    code:before, code:after {
+        content: '\`';
+    }
+
+    .logo-wrapper {
+        display: flex;
+        align-items: center;
+        align-content: center;
+        justify-content: center;
+        justify-items: center;
+    }
+
+    .logo {
+        margin: 0 75px;
+    }
+
+    .plus {
+        color: #BBB;
+        font-family: Times New Roman, Verdana;
+        font-size: 100px;
+    }
+
+    .spacer {
+        margin: 150px;
+    }
+
+    .emoji {
+        height: 1em;
+        width: 1em;
+        margin: 0 .05em 0 .1em;
+        vertical-align: -0.1em;
+    }
+    
+    .heading {
+        font-family: 'Inter', sans-serif;
+        font-size: 100%;
+        font-style: normal;
+        color: ${foreground};
+        line-height: 1.2;
+    }
+    
+    .heading2 {
+        font-family: 'Inter', sans-serif;
+        font-size: 60%;
+        font-style: normal;
+        color: ${foreground};
+        line-height: 1.2;
+    }
+    `;
+}
+
+export function getHtml(parsedReq: ParsedRequest) {
+    const { text, theme, md, fontSize, title } = parsedReq;
+    return `<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <title>Generated Image</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        ${getCss(theme, fontSize)}
+    </style>
+    <body>
+        <div class="wrap">
+            <div class="spacer">
+            <div class="spacer">
+            <div class="heading">${text!== undefined && emojify(
+                md ? marked(text) : sanitizeHtml(text)
+            )}</div>
+            <div class="heading2">${title!== undefined && emojify(
+                md ? marked(title) : sanitizeHtml(title)
+            )}
+            </div>
+        </div>
+    </body>
+</html>`;
+}
+
